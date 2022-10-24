e27e7123a9362a58cfdce1c3719f50c2619fe8aa
Leon Fink <Leon.fink.lf@gmail.com>
2022-10-24 11:46:17 +0200

    test

---
+++ src/DataObject.java
@@ -0,0 +1,36 @@
+public class DataObject {
+
+    private int id;
+    private String text;
+    private int groupId;
+
+    public DataObject(int id, String text, int groupId) {
+        this.id = id;
+        this.text = text;
+        this.groupId = groupId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+}
---
+++ src/FileReader.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class FileReader {
+
+    private String filePath;
+    private List<DataObject> list;
+
+    public FileReader(String filepath, List<DataObject> output) {
+        filePath = filepath;
+        list = output;
+    }
+
+    public void parseFile(){
+
+    }
+
+}
