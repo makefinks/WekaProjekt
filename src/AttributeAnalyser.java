34d7f9abeacbb0ec817223e0b939322b090dd827
Leon Fink <leon.fink.lf@gmail.com>
2022-10-25 14:38:39 +0200

    Attribute Analyser

---
+++ src/AttributeAnalyser.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttributeAnalyser {
+
+    private ArrayList<DataObject> data;
+
+    public AttributeAnalyser(List<DataObject> data){
+        this.data = (ArrayList<DataObject>) data;
+    }
+
+    public void checkWordCount(){
+
+    }
+
+}
