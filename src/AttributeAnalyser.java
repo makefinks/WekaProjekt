3c046046e40929363b97c4e2140b13b7f6b5d170
makefinks <leon.fink.lf@gmail.com>
2022-10-27 21:55:13 +0200

    Attribute Idea

--- src/AttributeAnalyser.java
+++ src/AttributeAnalyser.java
@@ -4,11 +4,9 @@ import java.util.List;
 public class AttributeAnalyser {
 
     private ArrayList<DataObject> data;
-
     public AttributeAnalyser(List<DataObject> data){
         this.data = (ArrayList<DataObject>) data;
     }
-
     public void checkWordCount(){
 
     }
