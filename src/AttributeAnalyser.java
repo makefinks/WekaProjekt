4bbd689685684c2a7dc88b4e98771bbc4689d889
unknown <paul.blanky@gmx.de>
2022-10-29 13:07:34 +0200

    satz count in att analyzer

--- src/AttributeAnalyser.java
+++ src/AttributeAnalyser.java
@@ -10,5 +10,21 @@ public class AttributeAnalyser {
     public void checkWordCount(){
 
     }
-
+    public void aveSentenceLenght() {
+    	data.forEach((d)->{
+    		durchschnittlicheSatzlaenge(d.getText());
+    	});
+    }
+    
+    private void durchschnittlicheSatzlaenge(String text) {
+    	
+    	String[] saetze=text.split(".|:");
+    	int count=saetze.length;
+    	int sum=0;
+    	for(int i=0;i<count;i++) {
+    		sum+=saetze[i].length();
+    	}
+    	double avg=sum/count;
+    	
+    }
 }
